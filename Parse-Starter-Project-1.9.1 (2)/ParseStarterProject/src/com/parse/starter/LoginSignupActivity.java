4bdee527240b11f0455f7f27fb95a31651754acb
kt888 <karthikapurushothaman@Karthikas-MacBook-Pro.local>
2015-04-22 20:16:32 -0700

    1

---
+++ Parse-Starter-Project-1.9.1 (2)/ParseStarterProject/src/com/parse/starter/LoginSignupActivity.java
@@ -0,0 +1,88 @@
+package com.parse.starter;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.LogInCallback;
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+
+public class LoginSignupActivity extends Activity {
+    // Declare Variables
+    Button loginbutton;
+    Button signup;
+    String usernametxt;
+    String passwordtxt;
+    EditText password;
+    EditText username;
+
+    /** Called when the activity is first created. */
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Parse.initialize(this, "MpTpblv5HgufOlBxG3q4xdrKYskcA8rJbQLCozyW", "pa1yuxLCOjK3llXPeRzK9gpWlk8521AuzbOx3NEh");
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+        ParseACL.setDefaultACL(defaultACL, true);
+
+        // Get the view from main.xml
+        setContentView(R.layout.activity_login_signup);
+        // Locate EditTexts in main.xml
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+
+        // Locate Buttons in main.xml
+        loginbutton = (Button) findViewById(R.id.login);
+
+        // Login Button Click Listener
+        loginbutton.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Retrieve the text entered from the EditText
+                usernametxt = username.getText().toString();
+                passwordtxt = password.getText().toString();
+
+                // Send data to Parse.com for verification
+                ParseUser.logInInBackground(usernametxt, passwordtxt,
+                        new LogInCallback() {
+                            public void done(ParseUser user, ParseException e) {
+                                if (user != null) {
+                                    // If user exist and authenticated, send user to Welcome.class
+                                    Intent intent = new Intent(
+                                            LoginSignupActivity.this,
+                                            Welcome.class);
+                                    startActivity(intent);
+                                    Toast.makeText(getApplicationContext(),
+                                            "Successfully Logged in",
+                                            Toast.LENGTH_LONG).show();
+                                    finish();
+                                } else {
+                                    Toast.makeText(
+                                            getApplicationContext(),
+                                            "No such user exist, please signup",
+                                            Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(
+                                            LoginSignupActivity.this,
+                                            SignupActivity.class);
+                                    startActivity(intent);
+                                    finish();
+
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+}
---
+++ Parse-Starter-Project-1.9.1 (2)/ParseStarterProject/src/com/parse/starter/ParseApplication.java
@@ -0,0 +1,34 @@
+package com.parse.starter;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseCrashReporting;
+import com.parse.ParseUser;
+
+public class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        ParseCrashReporting.enable(this);
+
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        // Add your initialization code here
+
+        Parse.initialize(this,"MpTpblv5HgufOlBxG3q4xdrKYskcA8rJbQLCozyW", "pa1yuxLCOjK3llXPeRzK9gpWlk8521AuzbOx3NEh");
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
+
+    }
+}
