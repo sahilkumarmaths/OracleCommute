111961dca491d23fc22379dd521cf9e30c64a6be
sahilkumarmaths <sahilkumarmaths@gmail.com>
2015-04-26 17:45:27 -0700

    getcoordinates

--- OracleCommute/src/oraclecommute/Direction.java
+++ OracleCommute/src/oraclecommute/Direction.java
@@ -1,13 +1,62 @@
 package oraclecommute;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
+import java.nio.charset.StandardCharsets;
+import static java.nio.charset.StandardCharsets.UTF_8;
 import oraclecommute.Point;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Direction {
+        private static final String GEOCODE = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+        private static final String USER_AGENT = "Mozilla/5.0";
+        
+        public static Point getCoordinates(String addr) throws UnsupportedEncodingException, IOException, JSONException
+        {
+            Point pt = new Point();
+            
+            String encodedStr = URLEncoder.encode(addr, UTF_8.toString());
+            System.out.println(GEOCODE + encodedStr);
+            HttpURLConnection con = WebConnection.getConnection(GEOCODE + encodedStr);
+            
+            con.setRequestMethod("GET");
+
+            //add request header
+            con.setRequestProperty("User-Agent", USER_AGENT);
+            int responseCode = con.getResponseCode();
+            
+            BufferedReader in = new BufferedReader( new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            System.out.println(response);
+            
+            JSONArray arr;
+            JSONObject obj;
+            
+            obj = new JSONObject(response.toString());     //Total object
+            
+            arr = obj.getJSONArray("results");    //Results array
+            obj = arr.getJSONObject(0);           //Results arryy 0
+            
+            obj = obj.getJSONObject("geometry").getJSONObject("location");   //Geomery object
+            System.out.println(obj.toString());
+            pt.setX(obj.getDouble("lat"));
+            pt.setY(obj.getDouble("lng"));
+            return pt;
+        }
+        
 	public List<Point> parsePath(String jsonPath) throws JSONException{
     	List<Point> path = new ArrayList<Point>();
     	JSONObject obj = new JSONObject(jsonPath);
---
+++ OracleCommute/src/oraclecommute/Util.java
@@ -0,0 +1,13 @@
+/*
+ * This is the utility class containing all the standard random helper functions
+ */
+package oraclecommute;
+
+/**
+ *
+ * @author sahkumar
+ */
+public class Util {
+    
+   
+}
