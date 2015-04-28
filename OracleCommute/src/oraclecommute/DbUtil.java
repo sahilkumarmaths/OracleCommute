a60ab5ef63c00c7e0f6bb4399514ed7a9aeb9680
vkarappa1 <vkarappa1@gmail.com>
2015-04-28 15:31:13 -0700

    Departure timestamps

--- OracleCommute/src/oraclecommute/DbUtil.java
+++ OracleCommute/src/oraclecommute/DbUtil.java
@@ -8,7 +8,11 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 
 
@@ -26,7 +30,8 @@ public class DbUtil {
 		      "     i_phone => ?,                                      \n"   +
 		      "     i_addr => ?,                                      \n"   +
 		      "     i_email => ?,                                      \n"   +
-		      "     i_time_departure => ?,                                      \n"   +
+		      "     i_home_departure => ?,                                      \n"   +
+		      "     i_office_departure => ?,                                      \n"   +
 		      "     i_is_driver => ?);                                     \n"   +
 		      "END; ";
 			
@@ -42,6 +47,7 @@ public class DbUtil {
 		 conn = this.getConnection();
 		 CallableStatement cstmt = conn.prepareCall(CREATE_EMPLOYEE);
 			
+			
 		cstmt.setString(1, emp.getUsername());
 		cstmt.setString(2, emp.getPassword());
 		cstmt.setDouble(3, emp.getCoordx());
@@ -50,8 +56,9 @@ public class DbUtil {
 		cstmt.setDouble(6, emp.getPhone());
 		cstmt.setString(7, emp.getAddress());
 		cstmt.setString(8, emp.getEmail());
-		cstmt.setDate(9, emp.getTime_departure());
-		cstmt.setString(10, emp.isIs_driver()? "Y":"N");
+		cstmt.setTimestamp(9, emp.getHome_departure());
+        cstmt.setTimestamp(10, emp.getOffice_departure());
+		cstmt.setString(11, emp.isIs_driver()? "Y":"N");
 		cstmt.executeUpdate();
 		 
 	 }
@@ -94,5 +101,53 @@ public class DbUtil {
 		return conn;
 		
 	}
+	
+	public static void main(String args[])
+	{
+		
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat(
+				"yyyy-MM-dd hh:mm:ss");
+
+		java.util.Date parsedTimeStamp = null;
+		try {
+			parsedTimeStamp = dateFormat.parse("2014-08-22 15:02:51");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Timestamp timestamp = new Timestamp(parsedTimeStamp.getTime());
+		
+        
+        // create a java calendar instance
+        Calendar calendar = Calendar.getInstance();
+         
+        // get a java date (java.util.Date) from the Calendar instance.
+        // this java date will represent the current date, or "now".
+        java.util.Date currentDate = calendar.getTime();
+         
+        // now, create a java.sql.Date from the java.util.Date
+        Timestamp home_departure = new Timestamp(currentDate.getTime());
+		
+        Employee emp = new Employee();
+        emp.setUsername("username");
+        emp.setPassword("password");
+        emp.setAddress("address");
+        emp.setCoordx(123.0);
+        emp.setCoordy(123.0);
+
+        emp.setEmail("email");
+        emp.setName("name");
+        emp.setPhone(123.0);
+        emp.setHome_departure(timestamp);
+        emp.setOffice_departure(timestamp);
+        emp.setIs_driver(true);
+
+
+        DbUtil db = new DbUtil();
+        db.createEmployee(emp);
+	}
+	
 	
 }
